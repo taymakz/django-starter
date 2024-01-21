488145b5a62a6b13f73aad585ab1149b17dad749
TaymaZ <taymazak1382@gmail.com>
2024-01-21 22:10:06 +0330

    fix: fix api prefix url

--- src/config/api/urls.py
+++ src/config/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 
 admin_urls = [
-    path('api/admin/users/',
+    path('admin/users/',
          include(('config.apps.user.account.urls.admin', 'config.apps.user.account'), namespace='users_admin')),
 ]
 
 front_urls = [
-    path('api/users/',
+    path('users/',
          include(('config.apps.user.account.urls.front', 'config.apps.user.account'), namespace='users_front')),
 ]
 
